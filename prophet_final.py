b1c83e241fe3a2bc39b2486ffdcddef2ca3c820b
Eric Youn <serlaluz.128@gmail.com>
2020-07-05 13:34:30 +0800

    Update prophet_final.py

--- prophet_final.py
+++ prophet_final.py
@@ -43,6 +43,81 @@ def main():
   choice = st.selectbox("Select Activity", activity)  
   st.write('### General information')
   st.write('This is only an example of how you can use Facebook Prophet as a way to predict trends for Google Trend data and also stock predictions using Google Finance. This will help the user to visualize and make informed decisions. **Please do not take this as financial advice.** This is for education purposes only.')
+  
+  if choice == 'Stock Prediction':
+    '''This activity will allow the user to fetch stock data through an API and use that data to predict stock price
+    '''
+    st.info("Predict Stock with Facebook Prophet")
+
+    #Allows users to type in their stock ticker
+    st.write('### (1) Enter the stock ticker of the company you want to predict')
+    if st.button("Show Ticker Examples"):
+      st.json({
+        "Facebook" : "FB",
+        "Google" : "GOOGL",
+        "Apple" : "AAPL",
+        "Amazon" : "AMZN",
+        "Microsoft" : "MSFT",
+        "Tesla" : "TSLA"
+      })
+    company = st.text_input("Enter Name for example FB for facebook", 'FB')
+
+    #Allows users to enter the start and end date of the historical stock data
+    st.write('### (2) Enter the historical start date and end dates for your stock')
+    start = st.date_input('Enter Stock Start Date', datetime.date(2011,1,1))
+  
+    end = st.date_input('Enter Stock End Date', datetime.date(2020,1,1))
+    stock_info = get_stock(company, start, end)
+    
+
+    if st.button("Fetch Stock using Yahoo Finance"):
+      st.write(stock_info)
+    
+    #Download stock information
+    st.success('Download stock data if needed')   
+    csv_exp = stock_info.to_csv(index=False)
+    b64 = base64.b64encode(csv_exp.encode()).decode()  
+    href = f'<a href="data:file/csv;base64,{b64}">Download Stock Info,</a> right-click and save link as ** [ticker].csv**'
+    st.markdown(href, unsafe_allow_html=True)
+
+    #Clean Data, Keep only date and close, Remove [Open, High,  Low, Adj CLose, and Volume]
+    stock_info = stock_info.reset_index()    
+    stock_info[['ds','y']] = stock_info[['Date' ,'Adj Close']]
+    stock_info = stock_info[['ds','y']]
+    train_dataset = stock_info.copy()
+    prophet_basic = Prophet()
+    prophet_basic.fit(train_dataset)
+    st.write('### (3) Select how many days you would like to forecast')
+    st.warning('The longer the forecasted days, the less accurate the predictions are.')
+
+    #Allows the user to choose how many days they want to forecast
+    periods_inputs = st.slider('Select how many days you want to forecast in the future',
+    min_value = 1, max_value = 365)
+
+    #Allows the machine to predict
+    if st.button("Predict"):
+      future = prophet_basic.make_future_dataframe(periods=periods_inputs)
+   
+      forecast = prophet_basic.predict(future)        
+      fcst = forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']]
+      fig1=prophet_basic.plot(forecast)
+      """
+      ### Data Table
+      The table shows the predicted value(depicted as yhat) and the upper and lower variance confidence levels.
+      """     
+      st.write(fcst)
+      """
+      ### GRAPH1
+      The next visual show in black dots are the actual data points and blue line is the predicted line over the given time.
+      """          
+      st.write(fig1)
+
+      """
+      ### GRAPH 2
+      The graphs below show a high level trend of predicted values, day of week trends, and yearly trends, depending on the data. The blue line is the prediction line and the blue area represents the confidence intervals.
+      """
+      fig2 = prophet_basic.plot_components(forecast)
+      st.write(fig2)         
 
   if choice == 'Google Trends':
     '''This will activity will allow the user to upload Google Trend csv files and predict the trend
@@ -120,80 +195,6 @@ def main():
       href = f'<a href="data:file/csv;base64,{b64}">Download Forecast File,</a> right-click and save link as ** [forecasted dataset].csv**'
       st.markdown(href, unsafe_allow_html=True)
   
-  if choice == 'Stock Prediction':
-    '''This activity will allow the user to fetch stock data through an API and use that data to predict stock price
-    '''
-    st.info("Predict Stock with Facebook Prophet")
-
-    #Allows users to type in their stock ticker
-    st.write('### (1) Enter the stock ticker of the company you want to predict')
-    if st.button("Show Ticker Examples"):
-      st.json({
-        "Facebook" : "FB",
-        "Google" : "GOOGL",
-        "Apple" : "AAPL",
-        "Amazon" : "AMZN",
-        "Microsoft" : "MSFT",
-        "Tesla" : "TSLA"
-      })
-    company = st.text_input("Enter Name for example FB for facebook", 'FB')
-
-    #Allows users to enter the start and end date of the historical stock data
-    st.write('### (2) Enter the historical start date and end dates for your stock')
-    start = st.date_input('Enter Stock Start Date', datetime.date(2011,1,1))
-  
-    end = st.date_input('Enter Stock End Date', datetime.date(2020,1,1))
-    stock_info = get_stock(company, start, end)
-    
-
-    if st.button("Fetch Stock using Yahoo Finance"):
-      st.write(stock_info)
-    
-    #Download stock information
-    st.success('Download stock data if needed')   
-    csv_exp = stock_info.to_csv(index=False)
-    b64 = base64.b64encode(csv_exp.encode()).decode()  
-    href = f'<a href="data:file/csv;base64,{b64}">Download Stock Info,</a> right-click and save link as ** [ticker].csv**'
-    st.markdown(href, unsafe_allow_html=True)
-
-    #Clean Data, Keep only date and close, Remove [Open, High,  Low, Adj CLose, and Volume]
-    stock_info = stock_info.reset_index()    
-    stock_info[['ds','y']] = stock_info[['Date' ,'Adj Close']]
-    stock_info = stock_info[['ds','y']]
-    train_dataset = stock_info.copy()
-    prophet_basic = Prophet()
-    prophet_basic.fit(train_dataset)
-    st.write('### (3) Select how many days you would like to forecast')
-    st.warning('The longer the forecasted days, the less accurate the predictions are.')
-
-    #Allows the user to choose how many days they want to forecast
-    periods_inputs = st.slider('Select how many days you want to forecast in the future',
-    min_value = 1, max_value = 365)
-
-    #Allows the machine to predict
-    if st.button("Predict"):
-      future = prophet_basic.make_future_dataframe(periods=periods_inputs)
-   
-      forecast = prophet_basic.predict(future)        
-      fcst = forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']]
-      fig1=prophet_basic.plot(forecast)
-      """
-      ### Data Table
-      The table shows the predicted value(depicted as yhat) and the upper and lower variance confidence levels.
-      """     
-      st.write(fcst)
-      """
-      ### GRAPH1
-      The next visual show in black dots are the actual data points and blue line is the predicted line over the given time.
-      """          
-      st.write(fig1)
-
-      """
-      ### GRAPH 2
-      The graphs below show a high level trend of predicted values, day of week trends, and yearly trends, depending on the data. The blue line is the prediction line and the blue area represents the confidence intervals.
-      """
-      fig2 = prophet_basic.plot_components(forecast)
-      st.write(fig2)         
 
 if __name__ == '__main__':
   main()
