6d62858d3fdaa2c407e402993ee9e75ec88ec14d
Eric Youn <serlaluz.128@gmail.com>
2020-07-12 12:38:39 +0800

    Update prophet_final.py

--- prophet_final.py
+++ prophet_final.py
@@ -10,10 +10,13 @@ import time
 import matplotlib
 import matplotlib.pyplot as plt
 import seaborn as sns
-#import io, base64, os, json, re, glob
 import datetime
+from datetime import datetime
+from time import mktime
 from datetime import timedelta
 import yfinance as yf
+from pycoingecko import CoinGeckoAPI
+
 
 
 
@@ -33,16 +36,52 @@ def get_stock(company, start, end):
   data = yf.download(company, start, end) 
   return data
 
+def crypto_stat(crypto_name, currency, start1, end1):
+  cg = CoinGeckoAPI()
+  conv_from1 = time.mktime(time.strptime(start1, '%Y-%m-%d'))
+  conv_to1 = time.mktime(time.strptime(end1, '%Y-%m-%d'))
+  crypto = cg.get_coin_market_chart_range_by_id(crypto_name, 'usd', conv_from1, conv_to1)
+  historical=crypto['prices'][:]
+  return historical
+
+def get_crypto_list():
+  cg = CoinGeckoAPI()
+  c_list = cg.get_coins_list()
+  d = []
+  for i in c_list:
+    d.append(i['id'])
+  return d
+
+def get_currency():
+  cg = CoinGeckoAPI()
+  sc_list = cg.get_supported_vs_currencies()
+  s = []
+  for i in sc_list:
+    s.append(i)
+  return s
+
+def clean_crypto(df):
+  '''This function will clean the Google Trends CSV so that it can be inputted nicely inside Facebook Prophet
+  '''
+  trends = pd.read_csv(df, header=1)
+  st.write(trends)
+  trends.columns = ["ds", "y"]
+  trends['ds'] = pd.to_datetime(trends['ds'], errors='coerce')
+  trends = trends.sort_values('ds', ascending=True)
+  return trends
+
+
+
 
 def main():
   '''This is the main function to operate the app
   '''
   st.title('Forecast Time Series Data with Facebook Prophet')
   st.image("graph.png", use_column_width=True)  
-  activity = ['Stock Prediction', 'Google Trends']
+  activity = ['Cryptocurrency Prediction', 'Stock Prediction', 'Google Trends']
   choice = st.selectbox("Select Activity", activity)  
   st.write('### General information')
-  st.write('This is only an example of how you can use Facebook Prophet as a way to predict trends for Google Trend data and also stock predictions. This will help the user to visualize and understand how Time Series Machine Learning can be applied. **Please do not take any results as financial advice.** This is for education purposes only.')
+  st.write('This is only an example of how you can use Facebook Prophet as a way to predict trends for cryptocurrency predictions, stock predictions, and also google trends. This will help the user to visualize and understand how Time Series Machine Learning can be applied. **Please do not take any results as financial advice.** This is for education purposes only.')
   
   if choice == 'Stock Prediction':
     '''This activity will allow the user to fetch stock data through an API and use that data to predict stock price
@@ -194,6 +233,88 @@ def main():
       b64 = base64.b64encode(csv_exp.encode()).decode()
       href = f'<a href="data:file/csv;base64,{b64}">Download Forecast File,</a> right-click and save link as ** [forecasted dataset].csv**'
       st.markdown(href, unsafe_allow_html=True)
+
+  if choice == 'Cryptocurrency Prediction':
+      '''This activity will allow the user to fetch stock data through an API and use that data to predict stock price
+      '''
+      st.info("Predict Cryptocurrency with Facebook Prophet")
+
+      #Allows users to type in their stock ticker
+      st.write('### (1) Enter the cryptocurrency you want to predict')
+      
+      #this lets you select
+      call_id = get_crypto_list()
+      crypto_avail = st.selectbox("Check available cryptocurrency in the list", call_id) 
+
+      crypto_name = st.text_input("Enter cryptocurrency name in lowercase and no spaces", 'bitcoin')
+      currency_lists = get_currency()
+      currency = st.selectbox("Choose your currency", currency_lists)
+
+      #Allows users to enter the start and end date of the historical crypto currency data
+      st.write('### (2) Enter the historical start date and end dates for your cryptocurrency')
+      #start1 = st.date_input('Enter Cryptocurrency Start Date', datetime.date(2016,1,1))
+      start1 = st.text_input('Enter Cryptocurrency Start Date', '2011-09-11')
+    
+      #end1 = st.date_input('Enter Cryptocurrency End Date', datetime.date(2020,7,1))
+      end1 = st.text_input('Enter Cryptocurrency End Date', '2020-07-03')
+
+      crypto = crypto_stat(crypto_name, currency, start1, end1)
+      df = pd.DataFrame(crypto)
+
+      # #Clean Data, Keep only date and close, Remove [Open, High,  Low, Adj CLose, and Volume]
+      crypto_info = df.reset_index()    
+      crypto_info[['ds','y']] = crypto_info[[0 , 1]]
+      crypto_info = crypto_info[['ds','y']]
+
+      crypto_info['ds'] = pd.to_datetime(crypto_info['ds'], unit='ms')
+      
+      #allows users to see the data
+      if st.button("View Cryptocurrency Data"):
+        st.write(crypto_info)
+      
+      #Download stock information
+      st.success('Download Cryptocurrency data if needed')   
+      csv_exp = df.to_csv(index=False)
+      b64 = base64.b64encode(csv_exp.encode()).decode()  
+      href = f'<a href="data:file/csv;base64,{b64}">Download Stock Info,</a> right-click and save link as ** [ticker].csv**'
+      st.markdown(href, unsafe_allow_html=True)
+
+
+
+      train_dataset = crypto_info.copy()
+      prophet_basic = Prophet()
+      prophet_basic.fit(train_dataset)
+
+      st.write('### (3) Select how many days you would like to forecast')
+      st.warning('The longer the forecasted days, the less accurate the predictions are.')
+
+      #Allows the user to choose how many days they want to forecast
+      periods_inputs = st.slider('Select how many days you want to forecast in the future', min_value = 1, max_value = 365)
+
+      #Allows the machine to predict
+      if st.button("Predict"):
+        future = prophet_basic.make_future_dataframe(periods=periods_inputs)
+    
+        forecast = prophet_basic.predict(future)        
+        fcst = forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']]
+        fig1=prophet_basic.plot(forecast)
+        """
+        ### Data Table
+        The table shows the predicted value(depicted as yhat) and the upper and lower variance confidence levels.
+        """     
+        st.write(fcst)
+        """
+        ### GRAPH1
+        The next visual show in black dots are the actual data points and blue line is the predicted line over the given time.
+        """          
+        st.write(fig1)
+
+        """
+        ### GRAPH 2
+        The graphs below show a high level trend of predicted values, day of week trends, and yearly trends, depending on the data. The blue line is the prediction line and the blue area represents the confidence intervals.
+        """
+        fig2 = prophet_basic.plot_components(forecast)
+        st.write(fig2)        
   
 
 if __name__ == '__main__':
